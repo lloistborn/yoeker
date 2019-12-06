4ea1bc3864fe32dd64773dbe128a82ffc3bc0392
lloisborn <faisalmorensya@gmail.com>
2019-12-06 22:53:33 +0700

    init project :rocket:

---
+++ src/main/java/com/faisal/yoeker/yoeker/YoekerApplication.java
@@ -0,0 +1,13 @@
+package com.faisal.yoeker.yoeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoekerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoekerApplication.class, args);
+	}
+
+}
